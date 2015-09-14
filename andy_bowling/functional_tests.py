e888230a4bcb6c2af09a8b20ecf253802c029b06
Andy <andrew.r.bowling@gmail.com>
2015-09-14 10:42:20 -0400

    Basic view now returns minimal html

--- andy_bowling/functional_tests.py
+++ andy_bowling/functional_tests.py
@@ -3,26 +3,26 @@ import unittest
 
 class NewVisitorTest(unittest.TestCase):
 
-    def setUp(self):
-        self.browser = webdriver.Firefox()
+	def setUp(self):
+		self.browser = webdriver.Firefox()
 
-    def tearDown(self):
-        self.browser.quit()
+	def tearDown(self):
+		self.browser.quit()
 		self.browser.implicitly_wait(3)
 
-    def test_can_start_a_list_and_retrieve_it_later(self):
-        # Bob from GreatCompany Inc. has heard about this awesome dev
+	def test_can_navigate_website(self):
+		# Bob from GreatCompany Inc. has heard about this awesome dev
 		# and wants to learn about him.
-        self.browser.get('http://localhost:8000')
+		self.browser.get('http://localhost:8000')
 
-        # He notices the page title and thinks "oh, how appropriate!"
-        self.assertIn('Home - Andy Bowling', self.browser.title)
-        self.fail('Finish the test!')
+		# He notices the page title and thinks "oh, how appropriate!"
+		self.assertIn('Home - Andy Bowling', self.browser.title)
+		self.fail('Finish the test!')
 
-        # He would like to learn more about Andy's database project.
+		# He would like to learn more about Andy's database project.
 		# He clicks the link and is brought to a new page
-		
+
 		# Satisfied, Bob returns to the home page
 
 if __name__ == '__main__':
-    unittest.main(warnings='ignore')
+	unittest.main(warnings='ignore')
