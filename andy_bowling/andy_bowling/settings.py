8329aec5fe34c99c6661181df127af9ec172a418
Andy <andrew.r.bowling@gmail.com>
2015-09-30 19:48:13 -0400

    Website completed locally, ready to host

--- andy_bowling/andy_bowling/settings.py
+++ andy_bowling/andy_bowling/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'yb5k5)-5nx=5bzm_edc+avh^vie4hee47%&zp8k6%5ghx#b@do'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
